41239889c9d9071212f7802dfac3968b922103c9
Wenyan <huwy.vio@gmail.com>
2014-11-05 19:09:14 +0800

    我TM居然conflict了

--- cs2102/searchTicket.php
+++ cs2102/searchTicket.php
@@ -6,13 +6,15 @@ CSS: <div class= \"UserInfoHeader\">
 <?php
   putenv('ORACLE_HOME=/oraclient');
   $dbh = ocilogon ('A0119541','crse1410','sid3');
-
+?>
+    
+      
+<?php
   if($_POST["UserName"]==null&&$_POST["UserID"]==null){
     echo "alert(\"Please Enter User Name or User ID!\");";
 
   }
   if( $_POST["UserName"] || $_POST["UserID"] ){
-<<<<<<< HEAD
       $sql = "select T.SUBSCRIBERID,S.USERNAME,O.MOVIETITLE AS MovieTile,Cn.NAME as CinemaName,Cn.LOCATION,H.HALLID,T.STARTTIME,T.ENDTIME 
       from Ticket T , Occupy O, Subscriber S, Cinema Cn, Hall H
       where 
@@ -43,32 +45,6 @@ CSS: <div class= \"UserInfoHeader\">
   }  
 
 }
-=======
-    $sql = "SELECT DISTINCT * from TICKET WHERE UID = ".$_POST["UserID"];
-    $stid = oci_parse ($dbh,$sql);
-    oci_execute($stid,OCI_DEFAULT);
-    $nrows = oci_fetch_all($stid, $results);
-    if($nrows == 0){
-      echo "Sorry, you havenot book any ticket yet, start ".
-            "<p> <a href=\"main.html/\">Book Now !</a>
-            </p>" ;
-      return;
-    }
-    foreach ($results as $key => $value) {
-      echo  "<tr>". 
-            "<td>".$key[""]."</td>".
-            "<td>".$key[""]."</td>".
-            "<td>".$key[""]."</td>".
-            "<td>".$key[""]."</td>".
-            "<td>".$key[""]."</td>".
-            "</tr>";
-          }
-
-    // while($row = oci_fetch_array($stid,oci_n){
-    // echo 
-    // } 
-  }
->>>>>>> FETCH_HEAD
   oci_free_statement($stid);
 ?>
           
