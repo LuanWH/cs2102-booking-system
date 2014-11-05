b11611c58fc39ca260ebca7e7476da351da8b2d5
Wenyan <huwy.vio@gmail.com>
2014-11-05 13:03:59 +0800

    SearchTicket.php edited

--- cs2102/searchTicket.php
+++ cs2102/searchTicket.php
@@ -7,16 +7,33 @@
       
 <?php
   if($_POST["UserName"]==null&&$_POST["UserID"]==null){
-    echo "alert(\"Please Enter User Name or User ID!\");"
+    echo "alert(\"Please Enter User Name or User ID!\");";
 
   }
   if( $_POST["UserName"] || $_POST["UserID"] ){
   $sql = "SELECT DISTINCT * from TICKET WHERE UID = ".$_POST["UserID"];
   $stid = oci_parse ($dbh,$sql);
   oci_execute($stid,OCI_DEFAULT);
-  while($row = oci_fetch_array($stid)){
-  echo 
-  } 
+  $nrows = oci_fetch_all($stid, $results);
+  if($nrows == 0){
+    echo "Sorry, you havenot book any ticket yet, start ".
+          "<p> <a href=\"main.html/\">Book Now !</a>
+          </p>" ;
+    return;
+  }
+  foreach ($results as $key => $value) {
+    echo  "<tr>". 
+          "<td>".$key[""]."</td>".
+          "<td>".$key[""]."</td>".
+          "<td>".$key[""]."</td>".
+          "<td>".$key[""]."</td>".
+          "<td>".$key[""]."</td>".
+          "</tr>";
+        }
+
+  // while($row = oci_fetch_array($stid,oci_n){
+  // echo 
+  // } 
 }
   oci_free_statement($stid);
 ?>
