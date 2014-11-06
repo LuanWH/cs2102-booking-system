28e00076ed14e025f8d2142e7f81cb4e383ddd61
LuanWH <lwhluan@gmail.com>
2014-11-06 15:39:36 +0800

    get fetchtimeslots work

--- cs2102/fetchTimeslots.php
+++ cs2102/fetchTimeslots.php
@@ -4,15 +4,7 @@
 
   	$cName = $_POST["cinemaName"];
   	$mName = $_POST["movieTitle"];
-  	$sql = 'SELECT DISTINCT TO_CHAR(O.STARTTIME, \'dd/mm/yyyy\') , 
-  							TO_CHAR(O.ENDTIME, \' dd/mm/yyyy\'), 
-  							O.HALLID 
-  							FROM OCCUPY O, HALL H, CINEMA C 
-  							WHERE O.MOVIETITLE="'.$mName.'" AND 
-  							O.HALLID = H.HALLID 
-  							AND H.NAMEOFCINEMA = C.NAME 
-  							AND H.LOCATIONOFCINEMA = C.LOCATION 
-  							AND C.NAME = "'.$cName.'";';
+  	$sql = "SELECT DISTINCT TO_CHAR(O.STARTTIME, 'dd/mm/yyyy') AS STARTTIME, TO_CHAR(O.ENDTIME, 'dd/mm/yyyy') AS ENDTIME, O.HALLID AS HALLID FROM OCCUPY O, HALL H, CINEMA C WHERE O.MOVIETITLE= '".$mName."' AND O.HALLID = H.HALLID AND H.NAMEOFCINEMA = C.NAME AND H.LOCATIONOFCINEMA = C.LOCATION AND C.NAME = '".$cName."'";
     $stid = oci_parse ($dbh,$sql); 
     oci_execute($stid, OCI_DEFAULT);
 
@@ -21,10 +13,9 @@
 	while($row = oci_fetch_array($stid)){
 		if($startBit != 1){
 			echo ',';
-		} else {
-			$startBit = 0;
-			echo '{"startTime": "'.$row['STARTTIME'].'", "endTime": "'.$row['ENDTIME'].'", "hallID": "'.$row['HALLID'].'""}';
-		}			
+		}
+		$startBit = 0;
+		echo '{"startTime": "'.$row['STARTTIME'].'", "endTime": "'.$row['ENDTIME'].'", "hallID": "'.$row['HALLID'].'"}';		
 	}
 	echo "]";
 	oci_free_statement($stid);
